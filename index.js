898a290a4162f54c8c76397b52d7cca10c1b07fa
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-13 14:51:01 +0500

    Sync

---
+++ index.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/playground').then(() => {
+    console.log("Connected to MongoDB");
+}).catch(err => {
+    console.error('Could Not Connect to MongoDB ', err)
+});
+
+
+
+const courseSchema=new mongoose.Schema({
+    name:String,
+    author:String,
+    tags:[String],
+    date: {type:Date,default:Date.now },
+    isPublished: Boolean
+
+})
+
+
+
+const Course=mongoose.model( 'Course',courseSchema);
+
+async function createCourse() {
+    const course=new Course({
+        name:'Angular Course',
+        author:'Talha',
+        tags:['angular','frontend'],
+        isPublished:true
+    })
+    
+    const result=await course.save();
+    console.log(result)
+    
+}
+
+
+async function getCourses(){
+    const courses=await Course.find({author:'Talha',isPublished:true}).limit(10).sort({name:1}).select({name:1,tag:1});
+    console.log(courses)
+
+}
+
+
+
+getCourses()
+createCourse()
+
