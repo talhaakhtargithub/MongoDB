c6192fbede7829e5b45924a54d45b7daa63e7c86
Talha Akhtar <talhaakhtarkpr1@gmail.com>
2024-01-13 21:31:02 +0500

    Sync

--- index.js
+++ index.js
@@ -7,35 +7,37 @@ mongoose.connect('mongodb://localhost:27017/playground').then(() => {
 
 
 
-const courseSchema=new mongoose.Schema({
-    name:String,
-    author:String,
-    tags:[String],
-    date: {type:Date,default:Date.now },
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
     isPublished: Boolean
 
 })
 
 
 
-const Course=mongoose.model( 'Course',courseSchema);
+const Course = mongoose.model('Course', courseSchema);
 
 async function createCourse() {
-    const course=new Course({
-        name:'Angular Course',
-        author:'Talha',
-        tags:['angular','frontend'],
-        isPublished:true
+    const course = new Course({
+        name: 'Angular Course',
+        author: 'Talha',
+        tags: ['angular', 'frontend'],
+        isPublished: true
     })
-    
-    const result=await course.save();
+
+    const result = await course.save();
     console.log(result)
-    
+
 }
+createCourse()
 
-
-async function getCourses(){
-    const courses=await Course.find({author:'Talha',isPublished:true}).limit(10).sort({name:1}).select({name:1,tag:1});
+async function getCourses() {
+    const courses = await Course
+    // .find({ author: 'Talha', isPublished: true })
+    .find({price:{$gt: 10}}).limit(10).sort({ name: 1 }).select({ name: 1, tag: 1 });
     console.log(courses)
 
 }
@@ -43,5 +45,5 @@ async function getCourses(){
 
 
 getCourses()
-createCourse()
+
 
